3547fd4facd89e6747c3223303941a6b92c45abf
tronghieu2810 <harrisondoan2810@gmail.com>
2021-12-28 16:46:50 +0700

    update

---
+++ Module/Webcam_Module.py
@@ -0,0 +1,31 @@
+import cv2
+
+width = 1920
+height = 1080
+
+gst_str = ('nvarguscamerasrc ! ' + 'video/x-raw(memory:NVMM), ' +
+          'width=(int)1920, height=(int)1080, ' +
+          'format=(string)NV12, framerate=(fraction)30/1 ! ' + 
+          'nvvidconv flip-method=2 ! ' + 
+          'video/x-raw, width=(int){}, height=(int){}, ' + 
+          'format=(string)BGRx ! ' +
+          'videoconvert ! appsink').format(width, height)
+
+cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER) 
+
+
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow('Input', frame)
+
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
